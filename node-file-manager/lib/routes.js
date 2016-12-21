13978741a6a02fa2f455bdd9b50decd730a97105
Enrique Sampaio dos Santos <developer@enriquesampaio.com>
2016-12-21 02:10:12 -0200

    Devolvendo file manager

---
+++ node-file-manager/lib/routes.js
@@ -0,0 +1,129 @@
+var fs = require('co-fs');
+var path = require('path');
+var views = require('co-views');
+var origFs = require('fs');
+var koaRouter = require('koa-router');
+var bodyParser = require('koa-bodyparser');
+var formParser = require('co-busboy');
+var request = require('request');
+
+var Tools = require('./tools');
+var FilePath = require('./fileMap').filePath;
+var FileManager = require('./fileManager');
+
+var api = 'http://localhost:3000/api/'
+
+var router = new koaRouter();
+var render = views(path.join(__dirname, './views'), { map: { html: 'ejs' } });
+
+router.get('/', function* () {
+  this.redirect('files');
+});
+
+router.get('/files', function* () {
+  this.body = yield render('files');
+});
+
+router.get('/api/(.*)', Tools.loadRealPath, Tools.checkPathExists, function* () {
+  var p = this.request.fPath;
+  var stats = yield fs.stat(p);
+  if (stats.isDirectory()) {
+    this.body = yield* FileManager.list(p);
+  }
+  else {
+    //this.body = yield fs.createReadStream(p);
+    this.body = origFs.createReadStream(p);
+  }
+});
+
+router.del('/api/(.*)', Tools.loadRealPath, Tools.checkPathExists, function* () {
+  var p = this.request.fPath;
+  yield* FileManager.remove(p);
+
+  request.post({ url: api + 'removeLynk', form: { path: p.split('myDropboxFolder/')[1] } }, function (err, httpResponse, body) { });
+
+  this.body = 'Delete Succeed!';
+});
+
+router.put('/api/(.*)', Tools.loadRealPath, Tools.checkPathExists, bodyParser(), function* () {
+  var type = this.query.type;
+  var p = this.request.fPath;
+  if (!type) {
+    this.status = 400;
+    this.body = 'Lack Arg Type'
+  }
+  else if (type === 'MOVE') {
+    var src = this.request.body.src;
+    if (!src || !(src instanceof Array)) return this.status = 400;
+    var src = src.map(function (relPath) {
+      return FilePath(relPath);
+    });
+    yield* FileManager.move(src, p);
+
+    request.post({ url: api + 'removeLynk', form: { path: src.split('myDropboxFolder/')[1] } }, function (err, httpResponse, body) { });
+    request.post({ url: api + 'createLynk', form: { path: p.split('myDropboxFolder/')[1] } }, function (err, httpResponse, body) { });
+
+    this.body = 'Move Succeed!';
+  }
+  else if (type === 'RENAME') {
+    var target = this.request.body.target;
+    if (!target) return this.status = 400;
+    yield* FileManager.rename(p, FilePath(target));
+
+    request.post({ url: api + 'removeLynk', form: { path: target.split('myDropboxFolder/')[1] } }, function (err, httpResponse, body) { });
+    request.post({ url: api + 'createLynk', form: { path: p.split('myDropboxFolder/')[1] } }, function (err, httpResponse, body) { });
+
+    this.body = 'Rename Succeed!';
+  }
+  else if (type === 'ARCHIVE') {
+    var src = this.request.body.src;
+    var archive = (p === '.' ? '' : p) + this.request.body.archive;
+    if (!src) return this.status = 400;
+    yield* FileManager.archive(C.data.root, archive, src, !!this.request.body.dirs);
+
+    request.post({ url: api + 'createLynk', form: { path: archive.split('myDropboxFolder/')[1] } }, function (err, httpResponse, body) {});
+
+    this.body = 'Create Archive Succeed!';
+  }
+  else {
+    this.status = 400;
+    this.body = 'Arg Type Error!';
+  }
+});
+
+router.post('/api/(.*)', Tools.loadRealPath, Tools.checkPathNotExists, function* () {
+  var type = this.query.type;
+  var p = this.request.fPath;
+  if (!type) {
+    this.status = 400;
+    this.body = 'Lack Arg Type!';
+  }
+  else if (type === 'CREATE_FOLDER') {
+    yield* FileManager.mkdirs(p);
+
+    request.post({ url: api + 'createLynk', form: { path: p.split('myDropboxFolder/')[1] } }, function (err, httpResponse, body) {});        
+
+    this.body = 'Create Folder Succeed!';
+  }
+  else if (type === 'UPLOAD_FILE') {
+    var formData = yield formParser(this.req);
+    if (formData.fieldname === 'upload') {
+      var writeStream = origFs.createWriteStream(p);
+      formData.pipe(writeStream);
+
+      request.post({ url: api + 'createLynk', form: { path: p.split('myDropboxFolder/')[1] } }, function (err, httpResponse, body) {});
+
+      this.body = 'Upload File Succeed!';
+    }
+    else {
+      this.status = 400;
+      this.body = 'Lack Upload File!';
+    }
+  }
+  else {
+    this.status = 400;
+    this.body = 'Arg Type Error!';
+  }
+});
+
+module.exports = router.middleware();
