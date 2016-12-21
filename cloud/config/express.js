ba1719f2ecbae01c5ef0d38ab55be426dc7402e1
Enrique Sampaio dos Santos <developer@enriquesampaio.com>
2016-12-21 02:49:16 -0200

    Aumentando limite de upload

--- cloud/config/express.js
+++ cloud/config/express.js
@@ -18,6 +18,7 @@ module.exports = function(app, config) {
 
   // app.use(favicon(config.root + '/public/img/favicon.ico'));
   app.use(logger('dev'));
+  app.use(express.limit('4mb'));
   app.use(fileUpload({
     limits: {
       fieldNameSize: 1000,
